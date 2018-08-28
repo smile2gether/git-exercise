499669b8bff2f35d3d076a7f6ab243c8602cc80a
Smile2gether <vlike2smile@gmail.com>
2018-08-28 11:52:23 +0700

    refactor code

--- craftNumber/CraftNumber.java
+++ craftNumber/CraftNumber.java
@@ -1,23 +1,38 @@
+import java.awt.List;
+import java.util.ArrayList;
+
 public class CraftNumber {
   public static void main (String [] args) {
+    int expectedCraftNumber = 6;
+
+    CraftNumber cn = new CraftNumber();
+    cn.findCraftNumber(expectedCraftNumber);
+  }
+
+  public void findCraftNumber (int expectedCraftNumber) {
     int count = 0;
-    for (long number = 6 ; number < 1000000000; number += 2) {
-      boolean isCraftNumber = isCraftNumber(number);
-      if (isCraftNumber && count < 5) {
-        count++;
-        System.out.println(number + " is craft number");
-      } else if (isCraftNumber && count == 5) {
+    for (long number = 2 ; number < 1000000000 ; number += 2) {
+      if (count == expectedCraftNumber) {
         break;
+      } else {
+        boolean isCraftNumber = isCraftNumber(number);
+        if (isCraftNumber) {
+          count++;
+          System.out.println(number + " is craft number");
+        }
       }
     }
   }
 
-  public static boolean isCraftNumber(long number) {
+  public boolean isCraftNumber(long number) {
     int result = 1;
+    ArrayList<Long> factorsList = new ArrayList<>();
 
-    for (long i = number - 1 ; i > 1; i--) {
-      if (number % i == 0) {
-        result += i;
+    for (long i = (number / 2) ; i > 1; i--) {
+      if (!factorsList.contains(i) && number % i == 0) {
+        result += i + (number / i);
+        factorsList.add(i);
+        factorsList.add(number/i);
       }
     }
 
