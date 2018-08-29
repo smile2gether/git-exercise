b66b2b9a76cf39a1215ec25fb5a246a4f30da6bd
Smile2gether <vlike2smile@gmail.com>
2018-08-29 14:27:42 +0700

    remove unused log

--- HelloCU/Hello.java
+++ HelloCU/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main (String[] args) {
         String name = "Kat";
-        long ID = 6181506326L;
+        long ID = 6181512026L;
         Hello hello = new Hello();
         String isValid = hello.validateStudentId(ID);
         System.out.println("ID : " + ID + isValid);
@@ -9,16 +9,15 @@ public class Hello {
 
     public String validateStudentId (long id) {
         String temp = Long.toString(id);
-        System.out.println(temp);
         long result = 0;
         int[] numbers = { 3, 1, 7, 2, 1, 3, 7 };
+        
         for (int i = 0; i < 7; i++)
         {
             System.out.println(temp.charAt(i));
-            System.out.println("=" + temp.charAt(i) + "*" + numbers[i] + "=" + ((int)temp.charAt(i) - '0') * numbers[i]);
             result += (temp.charAt(i) - '0') * numbers[i];
         }
-        System.out.println(result);
+
         return ((temp.charAt(7) - '0') == (result % 11)) ? " valid" : " invalid";
     }
 }
